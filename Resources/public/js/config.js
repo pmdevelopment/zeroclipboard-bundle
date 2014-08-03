5b2726a5f2a9e256db849bc3ea8ab1269663c339
Sven Joder <sj@pmdevelopment.de>
2014-08-03 14:41:55 +0200

    zeroclipboard functions

---
+++ Resources/public/js/config.js
@@ -0,0 +1,6 @@
+/* 
+ * @copyright Copyright by Promotional & Media Development GmbH
+ * @author Sven Joder sj@pmdevelopment.de
+ */
+
+ZeroClipboard.config( { swfPath: "/bundles/pmzeroclipboard/swf/ZeroClipboard.swf" } );
